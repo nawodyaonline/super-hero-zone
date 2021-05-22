deceeab04e7ae2a925e1a544ba955a577b3eeb8b
nawodyaonline <nawodyahckr@gmail.com>
2021-05-22 14:37:18 +0530

    Added config file

--- src/services/express.js
+++ src/services/express.js
@@ -1,14 +1,16 @@
 const express = require('express')
 
+const config = require("../config")
+
 const app = express()
 
 exports.start = () => {
-    app.listen(4041, (err) => {
+    app.listen(config.port, (err) => {
         if (err) {
             console.error(`Error : ${err}`);
             process.exit(-1);
         }
 
-        console.log("Super Hero zone API started on port 4041")
+        console.log(`Super Hero Zone API started at ${config.port}`)
     })
 }
